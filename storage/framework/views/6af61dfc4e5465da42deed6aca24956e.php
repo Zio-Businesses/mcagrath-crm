e567ecdb802765c0e13478f23b7cb0bc7155d053
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2024-11-27 15:38:43 +0530

    Merge

--- storage/framework/views/6af61dfc4e5465da42deed6aca24956e.php
+++
@@ -1,7 +0,0 @@
-
-<div class="col-12 px-0 pb-3 d-lg-flex d-md-flex d-block">
-    <p class="mb-0 text-lightest f-14 text-capitalize" style="width:40%;"><?php echo e($label); ?></p>
-    <div class="mb-0 text-dark-grey f-14 text-wrap ql-editor p-0"style="width:60%;"><?php echo nl2br($value); ?></div>
-</div>
-
-<?php /**PATH C:\xampp\htdocs\public_html\resources\views/components/cards/data-row-mod.blade.php ENDPATH**/ ?>
