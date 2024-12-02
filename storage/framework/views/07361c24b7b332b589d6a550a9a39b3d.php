2b129e129a00610b3974b9f6541e61d8fe3e3481
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2024-12-02 13:35:30 +0530

    config

--- storage/framework/views/07361c24b7b332b589d6a550a9a39b3d.php
+++
@@ -1,8 +0,0 @@
-<a href="<?php echo e($link); ?>" <?php echo e($attributes->merge(['class' => 'btn btn-secondary rounded f-14 p-2'])); ?>>
-    <?php if($icon != ''): ?>
-        <i class="fa fa-<?php echo e($icon); ?> mr-1"></i>
-    <?php endif; ?>
-    <?php echo e($slot); ?>
-
-</a>
-<?php /**PATH D:\Coding\crm-main\resources\views/components/forms/link-secondary.blade.php ENDPATH**/ ?>
